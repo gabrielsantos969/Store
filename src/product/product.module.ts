51937df25e6e880b21a01b253e9f983cd59583e8
gabrielsantos969 <gabrielsantos98898@gmail.com>
2025-03-07 23:13:08 -0300

    :wrench: Adjusted the envionment for docker and add PrismaModule

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,8 +3,10 @@ import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { ProductRepository } from './product.repository';
 import { ProductRepositoryInterface } from './product.repository.interface';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ProductController],
   providers: [
     ProductService,
