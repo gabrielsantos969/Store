3b73723b74875ecb9c3cd8803c68b0dd7c7b6a54
gabrielsantos969 <gabrielsantos98898@gmail.com>
2025-03-10 11:05:36 -0300

    :sparkles: Add Controller for Product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UsePipes } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ZodValidationPipe } from 'src/common/pipes/zod-validation.pipe';
+import { CreateProductDto, CreateProductSchema } from './dto/create-product.dto';
+import { Product } from '@prisma/client';
+import { UpdateProductDto, UpdateProductSchema } from './dto/update-product.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    constructor(private readonly service: ProductService){}
+
+    @Post()
+    @UsePipes(new ZodValidationPipe(CreateProductSchema))
+    async create(@Body() data: CreateProductDto): Promise<Product>{
+        return this.service.create(data);
+    }
+
+    @Get()
+    async findAll(): Promise<Product[]>{
+        return this.service.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id', ParseIntPipe) id: number): Promise<Product | null>{
+        return this.service.findOne(id);
+    }
+
+    @Put(':id')
+    @UsePipes(new ZodValidationPipe(UpdateProductSchema))
+    async update(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() data: UpdateProductDto,
+    ): Promise<Product>{
+        return this.service.update(id, data);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id', ParseIntPipe) id: number):Promise<Product>{
+        return this.service.remove(id);
+    }
+}
