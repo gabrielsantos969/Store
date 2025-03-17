92083ec0dfac5c412f4e4bb4a834f1406cb2bf47
gabrielsantos969 <gabrielsantos98898@gmail.com>
2025-03-17 16:27:52 -0300

    :wrench: Adjusted interface Address

--- src/address/address.repository.interface.ts
+++ src/address/address.repository.interface.ts
@@ -4,6 +4,6 @@ export interface IAddressRepository {
     create(data: Prisma.AddressCreateInput): Promise<Address>;
     findAllByCustomerId(customerId: string): Promise<Address[]>;
     findById(customerId: string, id: string): Promise<Address | null>;
-    update(customerId: string, id: string): Promise<Address>;
+    update(customerId: string, id: string, data: Prisma.AddressUpdateInput): Promise<Address>;
     remove(customerId: string, id: string): Promise<Address>;
 }
