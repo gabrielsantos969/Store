bc7bd12d98a329b2c7c47dd8b188444d20af3514
gabrielsantos969 <gabrielsantos98898@gmail.com>
2025-03-18 14:13:45 -0300

    :sparkles: Added Interface

---
+++ src/customer/customer.repository.interface.ts
@@ -0,0 +1,9 @@
+import { Customer, Prisma } from "@prisma/client";
+
+export interface  ICustomerRepository{
+    create(data: Prisma.CustomerCreateInput): Promise<Customer>;
+    findById(id: string): Promise<Customer | null>;
+    findByUserId(userId: string): Promise<Customer | null>;
+    update(userId: string, data: Prisma.CustomerUpdateInput): Promise<Customer>;
+    remove(userid: string): Promise<Customer>;
+}
