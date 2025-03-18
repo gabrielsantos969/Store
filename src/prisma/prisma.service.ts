9afb30241c5e7bd331d1a54e42feebc59ab38ae0
gabrielsantos969 <gabrielsantos98898@gmail.com>
2025-03-18 11:53:12 -0300

    :sparkles: Added middleware to Address in isDefault selection

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,6 +6,37 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
     
     async onModuleInit() {
         await this.$connect();
+        
+        this.$use(async (params, next) => {
+            
+            if (params.model === 'Address' && (params.action === 'create' || params.action === 'update')) {
+                const data = params.args.data;
+        
+                if (data.isDefault === true) {
+                    let customerId = data.customerId || data.Customer?.connect?.id;
+        
+                    if (!customerId && params.action === 'update' && params.args.where?.id) {
+        
+                        const address = await this.address.findUnique({
+                            where: { id: params.args.where.id },
+                            select: { customerId: true }
+                        });
+        
+                        customerId = address?.customerId;
+                    }
+        
+                    if (customerId) {
+                        await this.address.updateMany({
+                            where: { customerId },
+                            data: { isDefault: false }
+                        });
+                    }
+                }
+            }
+            
+            return next(params);
+        });
+        
     }
     
     async onModuleDestroy() {
