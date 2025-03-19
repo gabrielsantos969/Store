faa24c1a7364d9c1e36c66ed79e6fe93efe64fd4
gabrielsantos969 <gabrielsantos98898@gmail.com>
2025-03-19 11:14:40 -0300

    :sparkles: Adjusted DTOs with news parameters for Products

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,11 +1,18 @@
 import { z } from 'zod';
 import { UnitProduct } from './enum/unit-product.enum';
+import { FormatProduct } from './enum/format-product.enum';
 
 export const CreateProductSchema = z.object({
     name: z.string().min(3),
     description: z.string().optional(),
     price: z.number().min(0.01),
     imageUrl: z.string().optional(),
+    weight: z.number(),
+    height: z.number().min(2),
+    width: z.number().min(11),
+    length: z.number().min(16),
+    diameter: z.number().optional(),
+    format: z.nativeEnum(FormatProduct),
     categoryId: z.number().int().positive(),
     stock: z.object({
         quantity: z.number(),
--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,10 +1,17 @@
 import { z } from 'zod';
+import { FormatProduct } from './enum/format-product.enum';
 
 export const UpdateProductSchema = z.object({
     name: z.string().min(3).optional(),
     description: z.string().optional(),
     price: z.number().min(0.01).optional(),
     imageUrl: z.string().optional(),
+    weight: z.number().optional(),
+    height: z.number().min(2).optional(),
+    width: z.number().min(11).optional(),
+    length: z.number().min(16).optional(),
+    diameter: z.number().optional(),
+    format: z.nativeEnum(FormatProduct).optional(),
     categoryId: z.number().int().positive().optional()
 });
 
