60ce6cf756eb85c1a80e20f5fd333a5fd1fb686c
gabrielsantos969 <gabrielsantos98898@gmail.com>
2025-03-19 16:53:47 -0300

    :wrench: Adjusted paramter name cartId

--- src/cart/dto/add-cart-item.dto.ts
+++ src/cart/dto/add-cart-item.dto.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const AddCartItemSchema = z.object({
-    cardId: z.string(),
+    cartId: z.string(),
     quantity: z.number().min(1),
     productId: z.string(),
     total: z.number()
