16c0dcd065e71e801101b9a07dd065167d373daa
gabrielsantos969 <gabrielsantos98898@gmail.com>
2025-03-19 17:25:22 -0300

    :sparkles: Added Services in Cart

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { CartRepository } from './cart.repository';
+import { CartService } from './cart.service';
+import { ProductModule } from 'src/product/product.module';
+import { CustomerModule } from 'src/customer/customer.module';
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, ProductModule, CustomerModule],
     providers:[
         {
             provide: 'ICartRepository',
             useClass: CartRepository
-        }
+        },
+        CartService
     ]
 })
 export class CartModule {}
---
+++ src/cart/cart.service.ts
@@ -0,0 +1,82 @@
+import { Inject, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { ICartRepository } from './cart.repository.interface';
+import { Cart, CartItem } from '@prisma/client';
+import { ProductService } from 'src/product/product.service';
+import { CustomerService } from 'src/customer/customer.service';
+import { NotFoundError } from 'rxjs';
+
+@Injectable()
+export class CartService {
+    constructor(
+        @Inject('ICartRepository')
+        private readonly repository: ICartRepository,
+        private readonly product: ProductService,
+        private readonly customer: CustomerService
+    ){}
+
+    async addItem(userId: string, productId: string, quantity: number): Promise<CartItem>{
+        try {
+            const productExists = await this.product.findById(productId);
+            const customerExists = await this.customer.findByUserId(userId);
+    
+            const cart = await this.repository.createOrUpdateCart(customerExists.id);
+            const total = quantity * productExists!.price.toNumber();
+    
+            return this.repository.addOrUpdateCartItem({
+                cartId: cart.id,
+                productId: productExists!.id,
+                quantity,
+                total
+            });
+        } catch (error) {
+            if(error instanceof NotFoundError){
+                throw error;
+            }
+
+            throw new InternalServerErrorException(`${error.message}`);
+        }
+    }
+
+    async removeItem(userId: string, productId: string): Promise<any>{
+        try {
+            const cart = await this.getCart(userId);
+            await this.repository.removeCartItem(cart!.id, productId);
+
+            return { message: 'Item successfully removed' };
+        } catch (error) {
+            if(error instanceof NotFoundError){
+                throw error;
+            }
+
+            throw new InternalServerErrorException(`${error.message}`);
+        }
+
+    }
+
+    async getCart(userId: string): Promise<Cart | null>{
+        try {
+            const cart = await this.repository.findCartByCustomerId(userId);
+            if(!cart) throw new NotFoundException(`Cart not found.`);
+            return cart;
+        } catch (error) {
+            if(error instanceof NotFoundError){
+                throw error;
+            }
+
+            throw new InternalServerErrorException(`${error.message}`);
+        }
+    }
+
+    async clearCart(userId: string): Promise<Cart>{
+        try {
+            const customerExists = await this.customer.findByUserId(userId);
+            return this.repository.clearCart(customerExists.id);
+        } catch (error) {
+            if(error instanceof NotFoundError){
+                throw error;
+            }
+
+            throw new InternalServerErrorException(`${error.message}`);
+        }
+    }
+}
